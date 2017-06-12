f6af5aaeb3c925c95b0eb0d83d810fc092ffd879
xrr2016 <xiaoranran1993@163.com>
2017-06-12 23:18:13 +0800

    :zap:

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-var todo = 'Learning Typescript.';
-var name = 'leo';
+var container = document.getElementById('container');
+var todo = {
+    id: 123,
+    name: 'Learning TypeScript.',
+    completed: true
+};
+container.innerHTML = "\n  <div todo='" + todo.id + "' class=\"collection-item\">\n    <i class=\"" + (todo.completed ? 'blue-text text-darken-2' : 'hide') + " material-icons\">thumb_up</i>\n    <span class=\"name\">" + todo.name + "</span>\n  </div>";
--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
-const todo: string = 'Learning Typescript.'
-let name: string = 'leo'
+const container = document.getElementById('container')
+
+const todo = {
+  id: 123,
+  name: 'Learning TypeScript.',
+  completed: true
+}
+
+container.innerHTML = `
+  <div todo='${todo.id}' class="collection-item">
+    <i class="${todo.completed ? 'blue-text text-darken-2' : 'hide'} material-icons">thumb_up</i>
+    <span class="name">${todo.name}</span>
+  </div>`
